0e58c09202cd02ec7148c2f928fb7d84246af934
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2018-01-08 19:26:40 -0300

    Etapa 6: Inconclusa

--- js/script-index.js
+++ js/script-index.js
@@ -9,13 +9,7 @@ $(document).ready( function(){
 	renderActivities(activities);
 
 });
-
-
-/*
-* Función que se encarga de pintar TODAS las recetas que tengan 
-* marcado el atributo "highlighted" como TRUE
-*/
-
+/*Función que se encarga de pintar TODAS las recetas que tengan marcado el atributo "highlighted" como TRUE*/
 function renderHighlightedRecipes(recipesArray) {
 	//console.log('Recipes: ', recipesArray);
 	//console.log('Resultado: ', recipesArray[0].highlighted);
@@ -26,42 +20,39 @@ function renderHighlightedRecipes(recipesArray) {
 	  }
 	  function renderRecipe(recipe) {
 			//console.log('Voy a pintar la receta: ', [recipe]);
-			$('.list-recipes').append/*('<img src="img/recipes/320x350/' + recipesArray[i].name + '.jpg">')*/
+			$('.list-recipes').append/*('<img src="img/recipes/320x350/' + recipesArray[i].name + '.jpg">')para probar cargar solo la imagen*/
 			('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + 
 			recipesArray[i].title + '</span><span class="metadata-recipe"><span class="author-recipe">' + 
 			recipesArray[i].source.name + '</span><span class="bookmarks-recipe"><span class="icon-bookmark">'+ 
 			'</span></span></span></span>' + '<img src="img/recipes/320x350/' + recipesArray[i].name + '.jpg"/></a>');
 		}
-	}
-    
+	}  
 }
 
-
-/*
-* Función que se encarga de pintar todas las actividades
-*/
-
+/* Función que se encarga de pintar todas las actividades*/
 function renderActivities(activitiesArray) {
 	//console.log('Activities: ', activitiesArray);
 
 	for (var j = 0; j < activitiesArray.length; j++) {
-		console.log(j);
+		//console.log(j);
 	renderActivity(j);
 
 		function renderActivity(recipe) {
 		 if (recipe >= 0) {
 		 	$('.wrapper-message').hide();
+		 	$(.'list-activities').append('<a href="#" class="item-activity"><span class="attribution">' + 
+		 		'<span class="avatar"><img src="URL DE IMAGEN DE USUARIO (ATRIBUTO "userAvatar")" class="image-avatar">' +
+    			'</span><span class="meta"><span class="author">PRIMER NOMBRE DEL USUARIO (ATRIBUTO "userName")</span>' + 
+    			'made<span class="recipe">TITULO DE LA RECETA (ATRIBUTO "recipeName")</span>: TEXTO ACERCA DE LA RECETA (ATRIBUTO "text")' + 
+      			'<span class="location">&mdash;UBICACION DEL USUARIO (ATRIBUTO "place")</span></span></span>' +
+      			'<div class="bg-image" style="background-image: url('IMAGEN DE RECETA (ATTRIBUTO "image")');">' + 
+      			'</div></a>');
 		 }		
 	}	
 }
 }
 
 
-/*
-* Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-activity.html"
-*/
 
 
 
