d7c92d5f0acd5c56eb1e36e421fb04472ad22b29
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2018-01-09 11:45:33 -0300

    Etapa 8: funcionalidad a las pestañas para que aparezca una sección y desaparezcan las otras, y se revierta el proceso al apretar la otra pestaña

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,12 @@
 $(document).ready( function(){
 	$('.recipe .js-menu').hide(); //Ocultando menú en recipe.html
 	console.log("Documento listo");
-});
+});//final función.ready...no tocar
+
+$('.js-show-recipe').click( function(){
+	$('.page').removeClass('make');
+});//final función.click...no tocar
+
+$('.js-show-make').click( function(){
+	$('.page').addClass('make');
+});//final función.click...no tocar
